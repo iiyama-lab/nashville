64931d2d8e0e451820100e93611478b47494a531
Yuya Sumie <monamonacan0926@gmail.com>
2021-06-20 20:02:54 +0900

    Slack

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/DaikiYamakawa/deepl-go"
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape(client *deepl.Client) {
+type RequestBody struct {
+	Text string `json:"text,omitempty"`
+}
+
+func ExampleScrape(deeplClient *deepl.Client) {
 	// Request the HTML page.
 	res, err := http.Get("https://openaccess.thecvf.com/content/CVPR2021/html/Wu_Greedy_Hierarchical_Variational_Autoencoders_for_Large-Scale_Video_Prediction_CVPR_2021_paper.html")
 	if err != nil {
@@ -27,13 +34,44 @@ func ExampleScrape(client *deepl.Client) {
 		log.Fatal(err)
 	}
 
-	translateResponse, err := client.TranslateSentence(context.Background(), doc.Find("#abstract").Text(), "EN", "JA")
-	if err != nil {
-		fmt.Printf("Failed to translate text:\n   %+v\n", err)
-	} else {
-		fmt.Printf("%+v\n", translateResponse)
+	// translateResponse, err := client.TranslateSentence(context.Background(), doc.Find("#abstract").Text(), "EN", "JA")
+	// if err != nil {
+	// 	fmt.Printf("Failed to translate text:\n   %+v\n", err)
+	// } else {
+	// 	fmt.Printf("%+v\n", translateResponse)
+	// }
+
+	body := &RequestBody{
+		Text: "Hello, World!",
 	}
 
+	jsonString, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	endpoint := os.Getenv("SLACK_ENDPOINT")
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonString))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic("Error")
+	}
+
+	fmt.Printf("%#v", string(byteArray))
 	// Find the review items
 	doc.Find("#abstract").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the title
